567cdd2a7d5869d2c02ff16ff70bcbeeaa28ffda
han-lab <houhan@gmail.com>
2022-08-15 23:25:04 -0700

    change create_tables=True

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -11,7 +11,7 @@ if 'custom' not in dj.config:
     dj.config['custom'] = {}
 
 
-create_schema_settings = dict(create_schema=False, create_tables=False)
+create_schema_settings = dict(create_schema=False, create_tables=True)
 
 
 def get_schema_name(name):
